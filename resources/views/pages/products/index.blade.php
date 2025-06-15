bbdb7c8c80dff674076ef9414dfb39b858248ee3
Kentzie <onixd502@gmail.com>
2025-06-15 17:12:42 +0700

    table

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -14,5 +14,36 @@
 @endsection
 
 @section('content')
-    asas
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Product</th>
+                                <th>Description</th>
+                                <th>Kode</th>
+                                <th>Harga</th>
+                                <th>Stok</th>
+                                <th>Kategori</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>No</td>
+                                <td>Product</td>
+                                <td>Description</td>
+                                <td>Kode</td>
+                                <td>Harga</td>
+                                <td>Stok</td>
+                                <td>Kategori</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
