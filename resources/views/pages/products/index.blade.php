e1e473ec1375d77142142a74a4fcc9699d85c0a7
Kentzie <onixd502@gmail.com>
2025-06-15 16:53:13 +0700

    sidebar

---
+++ resources/views/pages/products/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('header')
+<div class="row mb-2">
+    <div class="col-sm-6">
+      <h1>Produk</h1>
+    </div>
+    <div class="col-sm-6">
+      <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active">Product</li>
+      </ol>
+    </div>
+@endsection
+
+@section('content')
+    asas
+@endsection
