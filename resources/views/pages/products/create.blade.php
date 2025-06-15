9dc06eb536cd1d1ac87a1a233df0c05d99461c60
Kentzie <onixd502@gmail.com>
2025-06-15 22:37:23 +0700

    CRUD

---
+++ resources/views/pages/products/create.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.main')
+
+@section('header')
+<div class="row mb-2">
+    <div class="col-sm-6">
+      <h1>Add Produk</h1>
+    </div>
+    <div class="col-sm-6">
+      <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active">Product</li>
+      </ol>
+    </div>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col">
+            {{-- @if (@errors->any()) --}}
+            {{-- @endif --}}
+            <form action="/products/store" method="POST">
+                @csrf
+                @method('POST')
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="name" class="form-label">Product Name</label>
+                            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value= "{{ old('name')}}">
+                            @error('name')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea name="description"
+                            id="description"
+                            cols="30"
+                            rows="10"
+                            class="form-control
+                            @error('description') is-invalid @enderror">{{ old('description') }}</textarea>
+                            @error('description')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="sku" class="form-label">Product Code</label>
+                            <input type="text" name="sku" id="sku" class="form-control @error('sku') is-invalid @enderror" value= "{{ old('sku')}}">
+                            @error('sku')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="number" inputmode= "numeric" name="price" id="price" class="form-control @error('price') is-invalid @enderror" value= "{{ old('price')}}">
+                            @error('price')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="stock" class="form-label">Stock</label>
+                            <input type="number" inputmode= "numeric" name="stock" id="stock" class="form-control @error('stock') is-invalid @enderror" value= "{{ old('stock')}}">
+                            @error('stock')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="stock" class="form-label">Category</label>
+                            <select name="category_id" id="category_id" class="form-control @error('category_id') is-invalid @enderror">
+                            @error('categoty_id')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                               @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                               @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-end">
+                            <a href="/products" class="btn btn-sm btn-outline-secondary mr-2">Cancel</a>
+                            <button type="submit" class="btn btn-sm btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -17,6 +17,11 @@
     <div class="row">
         <div class="col">
             <div class="card">
+                <div class="card-header d-flex justify-content-end">
+                    <a href="/products/create" class="btn btn-sm btn-primary">
+                        Add Product
+                    </a>
+                </div>
                 <div class="card-body">
                     <table class="table table-bordered">
                         <thead>
@@ -28,6 +33,7 @@
                                 <th>Price</th>
                                 <th>Stock</th>
                                 <th>Category</th>
+                                <th>#</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -35,11 +41,21 @@
                                 <tr>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $product->name }}</td>
-                                    <td>{{ $product->description }}</td>
+                                    <td>{{ $product->description ?? "-" }}</td>
                                     <td>{{ $product->sku }}</td>
                                     <td>{{ $product->price }}</td>
                                     <td>{{ $product->stock }}</td>
                                     <td>{{ $product->category->name }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="/products/edit/{{ $product->id }}" class="btn btn-sm btn-warning mr-2">Edit</a>
+                                        <form action="/products/{{ $product->id }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                                        </form>
+                                        </div>
+                                    </td>
                                 </tr>
                                 @endforeach
                         </tbody>
